bc48294c9ccc0e2d0cd97779cdd284a39df7d8e7
sdrangan <sdrangan@gmail.com>
2025-10-28 10:22:26 -0400

    Added synthesis analysis notebook

--- vector_mult/vmult_vitis/scripts/sweep_unroll.tcl
+++ vector_mult/vmult_vitis/scripts/sweep_unroll.tcl
@@ -1,4 +1,4 @@
-set unroll_factors {1 2 4}
+set unroll_factors {1 2 4 8}
 
 foreach uf $unroll_factors {
     set sol_name "sol_uf$uf"
@@ -6,12 +6,12 @@ foreach uf $unroll_factors {
     set_top vec_mult
 
     # Pass UNROLL_FACTOR as a macro
-    add_files -cflags "-Iinclude -DUNROLL_FACTOR=$uf" src/vmult.cpp
+    add_files -cflags "-Iinclude -DPIPELINE_EN=1 -DUNROLL_FACTOR=$uf" src/vmult.cpp
     add_files -tb testbench/tb_vmult.cpp
 
     open_solution $sol_name -reset
     set_part {xczu48dr-ffvg1517-2-e}
-    create_clock -period 10 -name default
+    create_clock -period 3.33 -name default
 
     # Optional: run C simulation
     # csim_design
@@ -24,5 +24,18 @@ foreach uf $unroll_factors {
     # Optional: export IP
     # export_design -format ip_catalog
 
+    # Construct full path to the XML report
+    set report_path "vmult_hls/$sol_name/syn/report/vec_mult_Pipeline_mult_loop_csynth.xml"
+
+    # Construct rclone copy command
+    set rclone_cmd "cp  $report_path ./vmult_reports/mult_loop_csynth_uf$uf.xml"
+
+    # Execute the shell command
+    exec sh -c $rclone_cmd
+
+    # Save and close the project
     close_project
+
 }
+
+exit
